7bd9be9b363759b48145ea26d3f37c753ad762a2
ColorScape <139798243+ColorScape@users.noreply.github.com>
2023-07-24 20:56:21 +0530

    Add files via upload

--- assets/js/templatemo-custom.js
+++ assets/js/templatemo-custom.js
@@ -134,3 +134,21 @@
 
 
 })(window.jQuery);
+
+
+var testimonials = [{
+  "name": "cederholm",
+  "attribution": "John Doe, Some Company",
+  "quote": "We\u2019ve been using <a href=\"https://www.apollowebstudio.com\">Apollo Web Studio</a> here at Company Name for years. The services offered have been indispensable to our little company."
+}, {
+  "name": "lisagor",
+  "attribution": "John Doe 2, Another Company",
+  "quote": "<a href=\"https://www.apollowebstudio.com\">Apollo Web Studio</a> is the web development company of the FUTURE.  The quality of work they provide for the price is just unmatched and blows the competition out of water or space in thee situation!"
+}]
+
+
+testimonial = testimonials[Math.floor(Math.random() * testimonials.length)];
+  document.getElementById("quote-quote").innerHTML = testimonial.quote
+  document.getElementById("quote-attrib").innerHTML = testimonial.attribution
+  document.getElementById("quote-wrapper").className = testimonial.name
+
